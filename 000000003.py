8bbe02d0981da8efe224984b8c9538c8bd28b0b7
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-04-15 17:05:33 -0600

    Add files via upload

---
+++ 000000003.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 12:09:23 2024
+
+@author: jperezr
+"""
+
+import streamlit as st
+
+
+st.title("PEDRO FUENTES SANTOS")
+
+st.header("NO SE ENCUENTRA EN LA:")
+st.subheader("") 
+st.subheader("") 
+st.header(" BASE DE DATOS NACIONAL DE PENSIONES NO RECLAMADAS")
+st.subheader("")          
+
+st.subheader("")
